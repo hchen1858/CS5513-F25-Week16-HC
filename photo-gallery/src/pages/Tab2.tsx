33415c4c1051bd927acb1539678dfd5aa9160ec6
Helen Chen <hchen1@bearcubs.santarosa.edu>
2025-12-09 21:02:31 -0800

    Page 1 of Tutorial complete

---
+++ photo-gallery/src/pages/Tab2.tsx
@@ -0,0 +1,34 @@
+import { camera } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon } from '@ionic/react';
+//import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+
+const Tab2: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+      {/* CHANGE: Add the floating action button */}
+      <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+
+        {/* <ExploreContainer name="Tab 2 page" /> */}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
